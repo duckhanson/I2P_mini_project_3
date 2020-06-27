e93e60572eb9bec28ab28524243cc05c7a54543a
duckhanson <ducklyu0301@gmail.com>
2020-06-27 13:41:19 +0800

    myAI with class othello, danger zone and stable spots.

--- player_infinite.cpp
+++ player_infinite.cpp
@@ -37,7 +37,8 @@ void write_valid_spot(std::ofstream& fout) {
     int n_valid_spots = next_valid_spots.size();
     srand(time(NULL));
     // Keep updating the output until getting killed.
-    while (true) {
+    int count = 100;
+    while (count--) {
         // Choose random spot. (Not random uniform here)
         int index = (rand() % n_valid_spots);
         Point p = next_valid_spots[index];
--- player_partial.cpp
+++ player_partial.cpp
@@ -26,194 +26,316 @@ class Point {
   }
 };
 
-int player;
-const int SIZE = 8;
-std::array<std::array<int, SIZE>, SIZE> board;
-std::vector<Point> next_valid_spots;
-std::vector<Point> alpha_next_valid_points;
-std::vector<Point> beta_next_valid_points;
-const std::array<Point, 8> directions{
-    {Point(-1, -1), Point(-1, 0), Point(-1, 1), Point(0, -1),
-     /*{0, 0}, */ Point(0, 1), Point(1, -1), Point(1, 0), Point(1, 1)}};
+class OthelloBoard {
+ public:
+  enum SPOT_STATE { EMPTY = 0, BLACK = 1, WHITE = 2 };
+  static const int SIZE = 8;
+  const std::array<Point, 8> directions{
+      {Point(-1, -1), Point(-1, 0), Point(-1, 1), Point(0, -1),
+       /*{0, 0}, */ Point(0, 1), Point(1, -1), Point(1, 0), Point(1, 1)}};
+  const std::array<Point, 4> corner_pts{{Point(0, 0), Point(0, SIZE - 1),
+                                         Point(SIZE - 1, 0),
+                                         Point(SIZE - 1, SIZE - 1)}};
+  const std::array<Point, 12> danger_zone{
+      {Point(0, 1), Point(1, 0), Point(1, 1), Point(0, SIZE - 2),
+       Point(1, SIZE - 1), Point(1, SIZE - 2), Point(SIZE - 1, 1),
+       Point(SIZE - 2, 0), Point(SIZE - 2, 1), Point(SIZE - 1, SIZE - 2),
+       Point(SIZE - 2, SIZE - 1), Point(SIZE - 2, SIZE - 2)}};
+  std::array<std::array<int, SIZE>, SIZE> board;
+  std::vector<Point> next_valid_spots;
+  std::array<int, 3> disc_count;
+  int cur_player, player;
 
-bool is_point_on_board(Point p) {
-  return 0 <= p.x && p.x < SIZE && 0 <= p.y && p.y < SIZE;
-}
-
-bool is_last_point() {
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
-      if (board[i][j] == 0) return false;
-    }
+ private:
+  int get_next_player(int player) const { return 3 - player; }
+  bool is_spot_on_board(Point p) const {
+    return 0 <= p.x && p.x < SIZE && 0 <= p.y && p.y < SIZE;
   }
-  return true;
-}
-
-bool is_point_valid(Point& center, bool alpha) {
-  if (board[center.x][center.y] != 0) return false;
-
-  int cur_player;
-  if (alpha)
-    cur_player = player;
-  else
-    cur_player = 3 - player;
-
-  int nxt_player = 3 - cur_player;
-
-  for (Point dir : directions) {
-    // Move along the direction while testing.
-    Point p = center + dir;
-    if (board[p.x][p.y] != nxt_player) continue;
-    p = p + dir;
-    while (is_point_on_board(p) && board[p.x][p.y] != 0) {
-      if (board[p.x][p.y] == cur_player) return true;
+  int get_disc(Point p) const { return board[p.x][p.y]; }
+  void set_disc(Point p, int disc) { board[p.x][p.y] = disc; }
+  bool is_disc_at(Point p, int disc) const {
+    if (!is_spot_on_board(p)) return false;
+    if (get_disc(p) != disc) return false;
+    return true;
+  }
+  bool is_end_of_the_game() const {
+    int disc_sum = disc_count[BLACK] + disc_count[WHITE] + disc_count[BLACK];
+    if (disc_sum == 64 || next_valid_spots.size() == 0) return true;
+    return false;
+  }
+  bool is_spot_valid(Point center) const {
+    if (get_disc(center) != EMPTY) return false;
+    for (Point dir : directions) {
+      // Move along the direction while testing.
+      Point p = center + dir;
+      if (!is_disc_at(p, get_next_player(cur_player))) continue;
       p = p + dir;
+      while (is_spot_on_board(p) && get_disc(p) != EMPTY) {
+        if (is_disc_at(p, cur_player)) return true;
+        p = p + dir;
+      }
+    }
+    return false;
+  }
+  bool is_spot_stable(Point center) const {
+    if (get_disc(center) == EMPTY || get_disc(center) == get_next_player(player)) return 0;
+    int dir_line = 0;
+    Point p;
+    for (Point dir : directions) {
+      p = center + dir;
+      while (is_disc_at(p, player))
+        p = p + dir;
+      if (!is_spot_on_board(p)) dir_line++;
+    }
+    if (dir_line > 3 && dir_line < 5)
+      return 1;
+    else
+      return 0;
+  }
+  void flip_discs(Point center) {
+    for (Point dir : directions) {
+      // Move along the direction while testing.
+      Point p = center + dir;
+      if (!is_disc_at(p, get_next_player(cur_player))) continue;
+      std::vector<Point> discs({p});
+      p = p + dir;
+      while (is_spot_on_board(p) && get_disc(p) != EMPTY) {
+        if (is_disc_at(p, cur_player)) {
+          for (Point s : discs) {
+            set_disc(s, cur_player);
+          }
+          disc_count[cur_player] += discs.size();
+          disc_count[get_next_player(cur_player)] -= discs.size();
+          break;
+        }
+        discs.push_back(p);
+        p = p + dir;
+      }
     }
   }
-  return false;
-}
 
-void get_valid_points(bool alpha) {
-  if (alpha) {
+ public:
+  OthelloBoard(std::ifstream& fin, std::ofstream& fout) {
+    reset(fin);
+    write_valid_spot(fout);
+  }
+  void reset(std::ifstream& fin) {
+    read_board(fin);
+    int empty_block = 0, black_block = 0, white_block = 0;
     for (int i = 0; i < SIZE; i++) {
       for (int j = 0; j < SIZE; j++) {
-        if (board[i][j] != 0) continue;
-        Point p = Point(i, j);
-        if (is_point_valid(p, true)) alpha_next_valid_points.push_back(p);
+        if (board[i][j] == EMPTY)
+          empty_block++;
+        else if (board[i][j] == BLACK)
+          black_block++;
+        else
+          white_block++;
       }
     }
-  } else {
+    disc_count[EMPTY] = empty_block;
+    disc_count[BLACK] = black_block;
+    disc_count[WHITE] = white_block;
+    read_valid_spots(fin);
+  }
+  void read_board(std::ifstream& fin) {
+    fin >> player;
     for (int i = 0; i < SIZE; i++) {
       for (int j = 0; j < SIZE; j++) {
-        if (board[i][j] != 0) continue;
+        fin >> board[i][j];
+      }
+    }
+  }
+  void read_valid_spots(std::ifstream& fin) {
+    size_t n_valid_spots;
+    fin >> n_valid_spots;
+    int x, y;
+    for (size_t i = 0; i < n_valid_spots; i++) {
+      fin >> x >> y;
+      next_valid_spots.push_back(Point(x, y));
+    }
+  }
+  std::vector<Point> get_valid_spots() const {
+    std::vector<Point> valid_spots;
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
         Point p = Point(i, j);
-        if (is_point_valid(p, false)) beta_next_valid_points.push_back(p);
+        if (board[i][j] != EMPTY) continue;
+        if (is_spot_valid(p)) valid_spots.push_back(p);
       }
     }
+    return valid_spots;
   }
-}
-
-int heuristic() {
-  int alpha_option, beta_option;
-  int alpha_on_the_board = 0;
-  int beta_on_the_board = 0;
-
-  get_valid_points(true);
-  alpha_option = alpha_next_valid_points.size();
-
-  get_valid_points(false);
-  beta_option = beta_next_valid_points.size();
-
-  for (int i = 0; i < SIZE; i++)
-    for (int j = 0; j < SIZE; j++)
-      if (board[i][j] == player)
-        alpha_on_the_board++;
-      else if (board[i][j] == 3 - player)
-        beta_on_the_board++;
-
-  return alpha_option * 0 - beta_option * 0 +
-         (alpha_on_the_board * 1 - beta_on_the_board * 0);
-}
-
-int alpha_beta(Point p, int depth, int alpha, int beta, bool maxPlayer) {
-  int value;
-  if (is_last_point()) {
-    value = heuristic();
-    return value;
+  void put_disc(Point p) {
+    set_disc(p, cur_player);
+    disc_count[cur_player]++;
+    disc_count[EMPTY]--;
+    flip_discs(p);
+    give_control_to_next_player();
   }
-  board[p.x][p.y] = player;
-  if (depth == 0 || is_last_point()) {
-    value = heuristic();
-    board[p.x][p.y] = 0;
-    return value;
+  void restore_disc(Point p) {
+    set_disc(p, EMPTY);
+    disc_count[cur_player]++;
+    disc_count[EMPTY]--;
+    give_control_to_next_player();
   }
-
-  if (maxPlayer) {
-    value = MIN;
-    get_valid_points(true);  // get valid for alpha player
-    for (auto np : alpha_next_valid_points) {
-      if (alpha_next_valid_points.size() > 10 && depth > 2)
-        value = std::max(value, alpha_beta(np, depth - 2, alpha, beta, false));
-      else
-        value = std::max(value, alpha_beta(np, depth - 1, alpha, beta, false));
-      alpha = std::max(alpha, value);
-      if (alpha >= beta) break;
+  void give_control_to_next_player() {
+    // Give control to the other player.
+    cur_player = get_next_player(cur_player);
+  }
+  int get_corner() {
+    int corner = 0;
+    int disc;
+    for (auto c : corner_pts) {
+      disc = get_disc(c);
+      if (disc == player) corner++;
+      else if (disc == get_next_player(player))
+        corner--;
     }
-  } else {
-    value = MAX;
-    get_valid_points(false);  // get valid for beta player
-    for (auto np : beta_next_valid_points) {
-      if (beta_next_valid_points.size() > 10 && depth > 2)
-        value = std::min(value, alpha_beta(np, depth - 2, alpha, beta, true));
-      else
-        value = std::min(value, alpha_beta(np, depth - 1, alpha, beta, true));
-      beta = std::min(beta, value);
-      if (alpha >= beta) break;
-    }
+    return corner;
   }
-  if (!alpha_next_valid_points.empty()) alpha_next_valid_points.clear();
-  if (!beta_next_valid_points.empty()) beta_next_valid_points.clear();
-  board[p.x][p.y] = 0;
-  return value;
-}
-
-void read_board(std::ifstream& fin) {
-  fin >> player;
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
-      fin >> board[i][j];
-    }
-  }
-}
-
-void read_valid_spots(std::ifstream& fin) {
-  int n_valid_spots;
-  fin >> n_valid_spots;
-  int x, y;
-  for (int i = 0; i < n_valid_spots; i++) {
-    fin >> x >> y;
-    next_valid_spots.push_back(Point(x, y));
-  }
-}
-
-void write_valid_spot(std::ofstream& fout) {
-  int value, child;
-  int alpha, beta;
-  Point p;
-  value = MIN, child = MIN, alpha = MIN, beta = MAX;
-  if (next_valid_spots.size() == 1)
-    p = next_valid_spots[0];
-  else {
-    for (auto np : next_valid_spots) {
-      // max_player's point of view
-      // race between depth(4) and depth(5), 4 wins always.
-      if (next_valid_spots.size() < 10)
-        child = alpha_beta(np, 4, alpha, beta, false);
-      else
-        child = alpha_beta(np, 3, alpha, beta, false);
-      // std::cout << child << std::endl;
-      alpha = std::max(alpha, child);
-      if (child > value) {
-        value = child;
-        p = np;
-        fout << np.x << " " << np.y << std::endl;
-        fout.flush();
+  int get_stable_spots() {
+    int s = 0;
+    for (size_t i = 0; i < SIZE; i++) {
+      for (size_t j = 0; j < SIZE; j++) {
+        s += is_spot_stable(Point(i, j));
       }
     }
+    return s;
   }
-  // Keep updating the output until getting killed.
-  while (true) {
-    fout << p.x << " " << p.y << std::endl;
-    fout.flush();
+  int get_danger_zone() {
+    int danger = 0;
+    int zone_count = 0;
+    int disc;
+    for (auto d : danger_zone) {
+      disc = get_disc(d);
+      zone_count++;
+      if (disc == player) {
+        if (is_disc_at(corner_pts[zone_count / 3], player)) {
+            danger--;
+        } else {
+          if (zone_count % 3 == 0)
+            danger += 10;
+          else
+            danger++;
+        }
+      }
+    }
+    return danger;
   }
-}
+  int heuristic(int depth) {
+    size_t alpha_option;
+    size_t beta_option;
+    if (cur_player == player) {
+      alpha_option = next_valid_spots.size();
+      cur_player = get_next_player(cur_player);
+      next_valid_spots = get_valid_spots();
+      beta_option = next_valid_spots.size();
+    } else {
+      beta_option = next_valid_spots.size();
+      cur_player = get_next_player(cur_player);
+      next_valid_spots = get_valid_spots();
+      alpha_option = next_valid_spots.size();
+    }
+
+    size_t alpha_on_the_board = disc_count[player];
+    // size_t beta_on_the_board = disc_count[get_next_player(player)];
+
+    // restore
+    cur_player = get_next_player(cur_player);
+    // minimize disc
+    int corner = get_corner();
+    if (corner < 1)  // first state : corner is important
+      return (depth * -1) * 2 + (beta_option) + get_corner() * 7 - get_danger_zone() * 11 + get_stable_spots() * 11;
+    else if (disc_count[EMPTY] > 39)  // second state : get second corner using stable line
+      return (depth / 2) + get_corner() * 5 - get_danger_zone() * 7 +
+             get_stable_spots() * 2;
+    else if (disc_count[EMPTY] > 21)  // third state : stable spread
+      return (depth * -1) * 2 + (alpha_option * 3 - beta_option * 2) / 2 +
+             get_corner() * 3 - get_danger_zone() * 13 +
+             get_stable_spots() * 7;
+    else    // fourth state : get maxium discs
+      return (depth * -1) * 2 + (alpha_on_the_board > 32) * 10 +
+             get_stable_spots() * 4;
+  }
+
+  int alpha_beta(Point p, int depth, int alpha, int beta, bool maxPlayer) {
+    // maxPlayer is player who wants to win the game.
+    put_disc(p);
+    int cur_size = next_valid_spots.size();
+    auto next_valid_points = get_valid_spots();
+    auto copy_board = board;
+    int value;
+    // End of alpha beta.
+    if (depth == 0 || cur_size == 1 || is_end_of_the_game()) {
+      value = heuristic(depth);
+      restore_disc(p);
+      board = copy_board;
+      return value;
+    }
+    int optimize_depth;
+    optimize_depth = depth - 1;
+
+    if (maxPlayer) {
+      value = MIN;
+      for (auto np : next_valid_points) {
+        value =
+            std::max(value, alpha_beta(np, optimize_depth, alpha, beta, false));
+        alpha = std::max(alpha, value);
+        if (alpha >= beta) break;
+      }
+    } else {
+      value = MAX;
+      for (auto np : next_valid_points) {
+        value =
+            std::min(value, alpha_beta(np, optimize_depth, alpha, beta, true));
+        beta = std::min(beta, value);
+        if (alpha >= beta) break;
+      }
+    }
+    // restore conditions
+    if (!next_valid_points.empty()) next_valid_points.clear();
+    restore_disc(p);
+    board = copy_board;
+    return value;
+  }
+
+  void write_valid_spot(std::ofstream& fout) {
+    int value, child;
+    int alpha, beta;
+    Point p;
+    value = MIN, child = MIN, alpha = MIN, beta = MAX;
+    if (next_valid_spots.size() == 1)
+      p = next_valid_spots[0];
+    else {
+      for (auto np : next_valid_spots) {
+        // max_player's point of view
+        // race between depth(4) and depth(5), 4 wins always.
+        if (next_valid_spots.size() < 13)
+          child = alpha_beta(np, 5, alpha, beta, false);
+        else
+          child = alpha_beta(np, 4, alpha, beta, false);
+        alpha = std::max(alpha, child);
+        if (child > value) {
+          value = child;
+          p = np;
+          fout << np.x << " " << np.y << std::endl;
+          fout.flush();
+        }
+      }
+    }
+    // Keep updating the output until getting killed.
+    size_t count = 100;
+    while (count--) {
+      fout << p.x << " " << p.y << std::endl;
+      fout.flush();
+    }
+  }
+};
 
 int main(int, char** argv) {
   std::ifstream fin(argv[1]);
   std::ofstream fout(argv[2]);
-  read_board(fin);
-  read_valid_spots(fin);
-  write_valid_spot(fout);
+  OthelloBoard B(fin, fout);
   fin.close();
   fout.close();
   return 0;
